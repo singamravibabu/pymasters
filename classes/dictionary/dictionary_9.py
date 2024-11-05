fbf8ec0bf464035940d02ee3e41b44eceaab6a0c
Singam Ravi Babu <thinkalgo.in@gmail.com>
2024-11-05 10:31:50 +0530

    created subfolders under class folder

--- classes/dictionary/dictionary_9.py
+++ classes/dictionary/dictionary_9.py
@@ -1,5 +1,6 @@
 # Avoid key error with dictionary using get() method
 # normally errors terminate the code from executing further
+# syntax: get(key_value[, default_value])
 
 text_to_num = {"one":1, "two":2, "three":3, "four":4, "five":5,
                "six":6, "seven":7, "eight":8, "nine":9, "ten":10}
